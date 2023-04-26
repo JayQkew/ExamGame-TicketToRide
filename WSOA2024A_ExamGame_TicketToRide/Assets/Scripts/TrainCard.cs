9041c162d650f11f81b466343f679dd6870ffbff
JayQkew <rayjebb@gmail.com>
2023-04-26 12:25:46 +0200

    Adding TrainCard Prefabs

---
+++ WSOA2024A_ExamGame_TicketToRide/Assets/Scripts/TrainCard.cs
@@ -0,0 +1,17 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TrainCard : MonoBehaviour
+{
+    [SerializeField] public string colour;
+    [SerializeField] public bool isLocomotive;
+    [SerializeField] public bool faceUp = true;
+    [SerializeField] TextMeshPro text;
+    void Start()
+    {
+        text.text = colour;
+        Debug.Log("Card colour is: "+ colour);
+        Debug.Log("Card faceUP: " + faceUp);
+    }
+}
