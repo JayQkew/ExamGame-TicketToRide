cdffae1a1bdb2d8a3fd327719faec35eb042964b
BIANCARIZZO <131270519+BIANCARIZZO@users.noreply.github.com>
2023-06-14 21:01:35 +0200

    Buttons

--- WSOA2024A_ExamGame_TicketToRide/Assets/Scripts/MenuButtons.cs
+++ WSOA2024A_ExamGame_TicketToRide/Assets/Scripts/MenuButtons.cs
@@ -5,6 +5,44 @@ using UnityEngine.SceneManagement;
 
 public class MenuButtons : MonoBehaviour
 {
+    public GameObject Main;
+    public GameObject Options;
+    public GameObject Credits;
+
+    public void Start()
+    {
+        Main.SetActive(true);
+        Options.SetActive(false);
+        Credits.SetActive(false);
+    }
+
+    public void OptionsBTN()
+    {
+        Main.SetActive(false);
+        Options.SetActive(true);
+        Credits.SetActive(false);
+    }
+
+    public void CreditsBTN()
+    {
+        Main.SetActive(false);
+        Options.SetActive(false);
+        Credits.SetActive(true);
+    }
+
+    public void OptionsExit()
+    {
+        Main.SetActive(true);
+        Options.SetActive(false);
+        Credits.SetActive(false);
+    }
+    public void CreditsExit()
+    {
+        Main.SetActive(true);
+        Options.SetActive(false);
+        Credits.SetActive(false);
+    }
+
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
